c0168b06f66525c81ec4765b9c5f81f072c0215d
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-08 09:58:32 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:todo_app/pages/home/home_page.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://fjxtnathtdvvqjsxffdt.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZqeHRuYXRodGR2dnFqc3hmZmR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjgzODg3MDQsImV4cCI6MjA0Mzk2NDcwNH0.0FExRouktbyOGWSx6aQhOivGp9aEcuk8zfJV1uD30fE',
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Task APP',
+      themeMode: ThemeMode.light,
+      theme: ThemeData.light(
+        useMaterial3: true,
+      ),
+      darkTheme: ThemeData.dark(
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
